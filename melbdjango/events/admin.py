dfa3e7e6dfdc974b331c90947f644b5628d8389d
Danielle Madeley <danielle@madeley.id.au>
2013-10-24 21:32:23 +1100

    Automatically set the author

--- melbdjango/events/admin.py
+++ melbdjango/events/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
 
-from .models import *
+from .models import Event
 
-admin.site.register(Event)
+
+class EventAdmin(admin.ModelAdmin):
+    """
+    Admin configuration for the Event model
+    """
+
+    exclude = ('author',)
+
+    def save_model(self, request, obj, form, change):
+        """
+        Save the Event model
+        """
+
+        obj.author = request.user
+
+        super(EventAdmin, self).save_model(request, obj, form, change)
+
+admin.site.register(Event, EventAdmin)
