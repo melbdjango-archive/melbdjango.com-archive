288d615f8d4c1a910c25806cb1f8b2520c3c0a59
Curtis Maloney <curtis@tinbrain.net>
2013-10-24 21:34:58 +1100

    Add models

--- melbdjango/hacks/models.py
+++ melbdjango/hacks/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+from datetime import datetime
+
+class Idea(models.Model):
+    title = models.CharField(max_length=2048)
+    description = models.TextField()
+
+    owner = models.ForeignKey('auth.User')
+
+    created = models.DateTimeField(default=datetime.now)
+
+class Vote(models.Model):
+    idea = models.ForeignKey('Idea')
+    user = models.ForeignKey('auth.User')
+
+    value = models.IntegerField(choices=(
+        (-1, 'Down'),
+        (1, 'Up'),
+    ))
+
+    class Meta:
+        unique_together = (
+            ('user', 'idea'),
+        )
+
+class Comment(models.Model):
+    idea = models.ForeignKey('Idea')
+    user = models.ForeignKey('auth.User')
+
+    created = models.DateTimeField(default=datetime.now)
+
+    comment = models.TextField()
+
+    class Meta:
+        ordering = ('created',)
