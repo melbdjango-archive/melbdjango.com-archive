d9e80b36b6b542632579f7c78363b5463054401d
Curtis Maloney <curtis@tinbrain.net>
2013-10-24 19:52:22 +1100

    Added basic apps

--- melbdjango/melbdjango/settings.py
+++ melbdjango/melbdjango/settings.py
@@ -36,6 +36,10 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'posts',
+    'events',
+    'hacks',
 )
 
 MIDDLEWARE_CLASSES = (
