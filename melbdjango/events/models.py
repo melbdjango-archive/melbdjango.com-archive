8f7897500616ac47e972dda94f2328bd61caf331
Danielle Madeley <danielle@madeley.id.au>
2013-10-24 21:20:38 +1100

    Add a title field, add unicode
    Add South

--- melbdjango/events/models.py
+++ melbdjango/events/models.py
@@ -11,6 +11,14 @@ class Event(TimeStampedModel):
 
     start_datetime = models.DateTimeField()
     finish_datetime = models.DateTimeField(null=True)
+
+    title = models.CharField(max_length=512)
     location_name = models.CharField(max_length=512)
     author = models.ForeignKey(User)
     content = models.TextField()
+
+    def __unicode__(self):
+        return "{title} @ {location} ({author})".format(
+            title=self.title,
+            location=self.location_name,
+            author=self.author.get_full_name() or self.author.username)
