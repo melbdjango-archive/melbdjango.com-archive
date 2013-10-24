79ee60fbeb6f901ab7434c8713246aa1a6016e26
Danielle Madeley <danielle@madeley.id.au>
2013-10-24 20:46:22 +1100

    Add a basic model for events

--- melbdjango/events/models.py
+++ melbdjango/events/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from django_extensions.db.models import TimeStampedModel
+
+
+class Event(TimeStampedModel):
+    """
+    An event
+    """
+
+    start_datetime = models.DateTimeField()
+    finish_datetime = models.DateTimeField(null=True)
+    location_name = models.CharField(max_length=512)
+    author = models.ForeignKey(User)
+    content = models.TextField()
