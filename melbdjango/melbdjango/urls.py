5530b27b2b9d42490dc7038fcb66096333ae3881
Daryl Antony <daryl@commoncode.com.au>
2013-10-25 15:32:51 +1100

    restore the hacks urls include

--- melbdjango/melbdjango/urls.py
+++ melbdjango/melbdjango/urls.py
@@ -7,6 +7,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'melbdjango.views.home', name='home'),
     url(r'^posts/', include('posts.urls')),
+    url(r'^hacks/', include('hacks.urls')),
     
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'django.shortcuts.render', {'template_name': 'index.html'}),
